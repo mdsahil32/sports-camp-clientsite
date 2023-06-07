0a3931b4535fa6c7ce4abfb9e5939b58a501c270
mdsahil32 <mohammedsahil3211@gmail.com>
2023-06-08 01:56:23 +0600

    set extrasection

---
+++ src/Pages/Home/ExtraSection/ExtraSection.jsx
@@ -0,0 +1,22 @@
+import './ExtraSection.css'
+
+const ExtraSection = () => {
+    return (
+        <div className='flex space-x-3'>
+            <div className='bg-football-team  h-[550px] my-32 md:py-40 bg-fixed  px-24'>
+                <div className=" text-center bg-black bg-opacity-60 py-2">
+                    <h3 className="md:text-6xl font-serif my-2 text-white">Our
+                        Kid Football Team.</h3>
+                </div>
+            </div>
+            <div className='bg-cricket-team  h-[550px] my-32 md:py-40 bg-fixed  px-24'>
+                <div className=" text-center bg-black bg-opacity-60 py-2">
+                    <h3 className="md:text-6xl font-serif text-white my-2">Our Kid Cricket Team.</h3>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default ExtraSection;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Banner/Banner";
+import ExtraSection from "../ExtraSection/ExtraSection";
 import PopularClasses from "../PopularClasses/PopularClasses";
 import PopularInstructors from "../PopularInstructors/PopularInstructors";
 
@@ -8,6 +9,7 @@ const Home = () => {
             <h1>this is home</h1>
             <Banner></Banner>
             <PopularClasses></PopularClasses>
+            <ExtraSection></ExtraSection>
             <PopularInstructors></PopularInstructors>
         </div>
     );
