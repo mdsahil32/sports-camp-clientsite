c1d7e8b64448bb2ff6d458f69ea726540d67aa3f
mdsahil32 <mohammedsahil3211@gmail.com>
2023-06-08 15:58:33 +0600

    set extra section

--- src/Pages/Home/ExtraSection/ExtraSection.jsx
+++ src/Pages/Home/ExtraSection/ExtraSection.jsx
@@ -2,7 +2,7 @@ import './ExtraSection.css'
 
 const ExtraSection = () => {
     return (
-        <div className='flex space-x-3'>
+        <div className='flex max-w-screen-lg mx-auto space-x-3'>
             <div className='bg-football-team  h-[550px] my-32 md:py-40 bg-fixed  px-24'>
                 <div className=" text-center bg-black bg-opacity-60 py-2">
                     <h3 className="md:text-6xl font-serif my-2 text-white">Our
